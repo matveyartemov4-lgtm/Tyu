f84ad64a038c4939ef81a34e9c8d7ddc057d5413
matveyartemov4-lgtm <matveyartemov4@gmail.com>
2026-06-04 17:53:53 +0400

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -23,7 +23,7 @@ from telethon.errors import FloodWaitError
 # ==========================================
 # 1. КОНФИГУРАЦИЯ
 # ==========================================
-BOT_TOKEN = "8932397702:AAGY7wdLp4dy96MeSH1lce86HqzClmmMEqk"
+BOT_TOKEN = "8932397702:AAH7C2-aJc0uSga6otNw-on4CBGIfRAzroQ"
 API_ID = 33248398
 API_HASH = "6543087387b7b14fcafcca74d28b1158"
 
